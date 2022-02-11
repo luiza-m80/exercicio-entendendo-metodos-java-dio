a7a65b2e48fecbe14b37e02e985809c02f34976d
luiza-m80 <luuhbam@gmail.com>
2022-02-11 09:58:50 -0300

    Second Commit

---
+++ src/sobrecarga/Main.java
@@ -0,0 +1,12 @@
+package sobrecarga;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Exercício do Quadrilátero: ");
+        Quadrilatero.area(4);
+        Quadrilatero.area(6, 8);
+        Quadrilatero.area(12, 6, 4);
+    }
+}
